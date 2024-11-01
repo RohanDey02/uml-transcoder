c26367354996b5a33c3e594900b47439476dae98
RohanDey02 <deyrohan15@gmail.com>
2024-11-01 17:18:37 -0400

    Adding more base files

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UmlDiagramComponent } from './components/uml-diagram/uml-diagram.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UmlDiagramComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
