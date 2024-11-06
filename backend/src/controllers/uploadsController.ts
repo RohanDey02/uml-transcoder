02d2fa8e093955bebcbe110fd4cb35cc8b1d3b6a
RohanDey02 <deyrohan15@gmail.com>
2024-11-06 09:42:54 -0500

    Added endpoint to wipe uploads/

--- backend/src/controllers/uploadsController.ts
+++ backend/src/controllers/uploadsController.ts
@@ -16,3 +16,26 @@ export const getLocalImage = (req: Request, res: Response): void => {
     res.sendFile(imagePath);
   });
 };
+
+// TODO: Attach to a cron job to wipe at ~4:00 AM daily, etc.
+export const wipeUploadsDir = (req: Request, res: Response): void => {
+  const uploadsDir = path.join(__dirname, '..', '..', 'uploads');
+
+  fs.readdir(uploadsDir, (err: any, files: string[]) => {
+    if (err) {
+      res.status(500).send('Unable to read uploads directory');
+      return;
+    }
+
+    for (const file of files) {
+      fs.unlink(path.join(uploadsDir, file), (err: any) => {
+        if (err) {
+          res.status(500).send('Error deleting files');
+          return;
+        }
+      });
+    }
+
+    res.send('Uploads directory wiped');
+  });
+};
