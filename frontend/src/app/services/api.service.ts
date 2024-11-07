bd2aadca14c1ad7cc4f61b7060fc4d30568d4c83
RohanDey02 <deyrohan15@gmail.com>
2024-11-07 12:49:11 -0500

    Created AI generation functions in API Service

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -11,9 +11,38 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   uploadFile(file: File) {
-    const formData = new FormData();
+    const formData: FormData = new FormData();
     formData.append('file', file);
 
     return this.http.post(`${this.endpoint}/api/v1/upload`, formData);
+  }
+
+  generateUMLToCode(artifactSrc: string, language: string) {
+    const prompt: string = `Take the UML diagram (attached as an image) and convert to ${language} code.`;
+
+    return this.http.post(`${this.endpoint}/api/v1/generate`, {
+      prompt,
+      artifactSrc,
+      purpose: "UML_TO_CODE"
+    });
+  }
+
+  generateUMLToJSON(artifactSrc: string) {
+    const prompt: string = 'Take this UML diagram (attached as an image) and convert to desired form.';
+
+    return this.http.post(`${this.endpoint}/api/v1/generate`, {
+      prompt,
+      artifactSrc,
+      purpose: "UML_TO_JSON",
+    });
+  }
+
+  generateCodeToJSON(language: string) {
+    const prompt: string = `Take this ${language} code and convert to desired form.`;
+
+    return this.http.post(`${this.endpoint}/api/v1/generate`, {
+      prompt,
+      purpose: "CODE_TO_JSON",
+    });
   }
 }
